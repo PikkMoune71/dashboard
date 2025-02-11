aa548a2b25f824b05be5e4e6f88bcb1d2b6ff3b0
PikkMoune71 <antoinepicard385@gmail.com>
2025-02-11 13:16:56 +0100

    fix: form login and register

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -18,6 +18,7 @@ import {
 } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export function LoginForm({
   className,
@@ -57,27 +58,29 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Login</CardTitle>
-          <CardDescription>Login with your account</CardDescription>
+          <CardTitle className="text-xl">Se connecter</CardTitle>
+          <CardDescription>
+            Connexion à la plateforme de Todo List
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleLogin} className="space-y-6">
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={handleGoogleLogin}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                  fill="currentColor"
-                />
-              </svg>
-              Login with Google
-            </Button>
+          <Button
+            variant="outline"
+            className="w-full mb-4"
+            onClick={handleGoogleLogin}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                fill="currentColor"
+              />
+            </svg>
+            Connexion avec Google
+          </Button>
+          <form onSubmit={handleLogin} className="space-y-2">
             <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
               <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                Or continue with
+                Ou continuer avec
               </span>
             </div>
             <div className="space-y-4">
@@ -93,7 +96,7 @@ export function LoginForm({
                 />
               </div>
               <div>
-                <Label htmlFor="password">Password</Label>
+                <Label htmlFor="password">Mot de passe</Label>
                 <Input
                   id="password"
                   type="password"
@@ -105,10 +108,16 @@ export function LoginForm({
               </div>
               {error && <p className="text-red-500 text-sm">{error.message}</p>}
               <Button type="submit" className="w-full" disabled={loading}>
-                {loading ? "Logging in..." : "Login"}
+                {loading ? "Connexion..." : "Se connecter"}
               </Button>
             </div>
           </form>
+          <p className="text-center text-sm text-muted-foreground">
+            Pas encore inscrit ?{" "}
+            <Link href="/register" className="text-primary">
+              S&apos;inscrire
+            </Link>
+          </p>
         </CardContent>
       </Card>
     </div>
